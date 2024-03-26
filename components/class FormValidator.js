5e059478f7461976d6aae9138bcf23f438e3c9bf
Obi-Pi-Kenobi <marcuscrawford91@gmail.com>
2024-03-26 12:24:08 -0500

    Add files via upload

---
+++ components/class FormValidator.js
@@ -0,0 +1,90 @@
+export default class FormValidator {
+    constructor(settings, formElement) {
+        this._inputSelector = settings.inputSelector;
+        this._submitButtonSelector = settings.submitButtonSelector;
+        this._inactiveButtonClass = settings.inactiveButtonClass;
+        this._inputErrorClass = settings.inputErrorClass;
+        this._errorClass = settings.errorClass;
+        this._form = formElement;
+    }
+
+    _showInputError(inputElement, errorMessage) {
+        const errorElement = this._form.querySelector(`#${inputElement.id}-error`);
+        inputElement.classList.add(this._inputErrorClass);
+        errorElement.textContent = errorMessage;
+        errorElement.classList.add(this._errorClass);
+    }
+
+    _hideInputError(inputElement) {
+        const errorElement = this._form.querySelector(`#${inputElement.id}-error`);
+        inputElement.classList.remove(this._inputErrorClass);
+        errorElement.textContent = "";
+        errorElement.classList.remove(this._errorClass);
+    }
+
+    _checkInputValidity(inputElement) {
+        if (!inputElement.validity.valid) {
+            this._showInputError(inputElement, inputElement.validationMessage);
+        } else {
+            this._hideInputError(inputElement);
+        }
+    }
+
+    _hasInvalidInput() {
+        return this._inputList.some((inputElement) => !inputElement.validity.valid);
+    }
+
+    _enableButton() {
+        this._submitButton.classList.remove(this._inactiveButtonClass);
+        this._submitButton.disabled = false;
+    }
+
+    disableButton() {
+        this._submitButton.classList.add(this._inactiveButtonClass);
+        this._submitButton.disabled = true;
+    }
+
+    toggleButtonState() {
+        if (this._hasInvalidInput()) {
+            this.disableButton();
+        } else {
+            this._enableButton();
+        }
+    }
+
+    _clearFormFields() {
+        this._form.reset();
+    }
+
+
+    _setEventListeners() {
+        this._inputList = Array.from(this._form.querySelectorAll(this._inputSelector));
+        this._submitButton = this._form.querySelector(this._submitButtonSelector);
+        this._inputList.forEach((inputElement) => {
+            inputElement.addEventListener("input", () => {
+                this._checkInputValidity(inputElement);
+                this.toggleButtonState();
+            });
+        });
+    }
+
+    enableValidation() {
+        this._form.addEventListener("submit", (evt) => {
+            evt.preventDefault();
+                this.disableButton();
+        });
+
+        this._setEventListeners();
+        this.toggleButtonState();
+
+    }
+
+
+    resetValidation() {
+        this._inputList.forEach((inputElement) => {
+            this._hideInputError(inputElement);
+        });
+        this.disableButton();
+    }
+}
+
